ec515bab0c99409894c3f02ad3ba92c520062add
Chris Malek <cmalek@caltech.edu>
2024-08-22 14:36:57 -0700

    FEATURE: added "botocraft shell" command
    This is either an iPython (if installed) or regular python interactive session with all the botocraft.services models loaded.

--- botocraft/cli/__init__.py
+++ botocraft/cli/__init__.py
@@ -2,3 +2,4 @@ from .cli import cli  # noqa:F401
 from .sync import *  # noqa:F403,F401
 from .inspect import *  # noqa:F403,F401
 from .data import *  # noqa:F403,F401
+from .shell import *  # noqa:F403,F401
