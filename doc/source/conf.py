0a46f4a2345d01cb0d5377f00f5a376b1eee589c
Chris Malek <cmalek@caltech.edu>
2025-06-10 16:22:05 -0700

    Bump version: 0.17.3 → 0.17.4

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -28,7 +28,7 @@ copyright: str = "Caltech IMSS ADS"  # noqa: A001
 author: str = "Caltech IMSS ADS"
 
 # The full version, including alpha/beta/rc tags
-release = "0.17.3"
+release = "0.17.4"
 
 
 # -- General configuration ---------------------------------------------------
