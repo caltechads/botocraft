ddf7ec62b45e0148cf63bfe0b3f990672a1d11a9
Chris Malek <cmalek@caltech.edu>
2026-02-06 11:56:36 -0800

    Bump version: 0.21.7 → 0.22.0

--- botocraft/__init__.py
+++ botocraft/__init__.py
@@ -1,1 +1,1 @@
-__version__: str = "0.21.7"
+__version__: str = "0.22.0"
--- doc/source/conf.py
+++ doc/source/conf.py
@@ -28,7 +28,7 @@ copyright: str = "Caltech IMSS ADS"  # noqa: A001
 author: str = "Caltech IMSS ADS"
 
 # The full version, including alpha/beta/rc tags
-release = "0.21.7"
+release = "0.22.0"
 
 
 # -- General configuration ---------------------------------------------------
