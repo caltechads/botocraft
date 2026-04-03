52e3beec2c1d1ba5cffeedd6426b56c09e6c87e0
Chris Malek <cmalek@caltech.edu>
2026-04-03 13:37:42 -0700

    FEATURE: documentdb support

---
+++ botocraft/mixins/docdb.py
@@ -0,0 +1,247 @@
+import json
+from collections.abc import Callable
+from functools import wraps
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from botocraft.services.abstract import PrimaryBoto3ModelQuerySet
+    from botocraft.services.common import Credentials
+    from botocraft.services.docdb import DocDBCluster
+    from botocraft.services.docdb import DocDBInstance
+    from botocraft.services.docdb import DocDBSubnetGroup
+    from botocraft.services.docdb import DocDBParameterGroup
+
+# ----------
+# Decorators
+# ----------
+
+
+def single_docdb_cluster_include_tags(
+    func: Callable[..., "DocDBCluster"],
+) -> Callable[..., "DocDBCluster"]:
+    """
+    Decorator to convert a :py:class:`botocraft.services.docdb.DocDBCluster` object
+    to a :py:class:`botocraft.services.docdb.DocDBCluster` object with tags included.
+    """
+
+    @wraps(func)
+    def wrapper(self, *args, **kwargs) -> "DocDBCluster":
+        response = func(self, *args, **kwargs)
+        tags = self.client.list_tags_for_resource(ResourceName=response.DBClusterArn)
+        response.Tags = tags["TagList"]
+        return response
+
+    return wrapper
+
+
+def multiple_docdb_cluster_include_tags(
+    func: Callable[..., "PrimaryBoto3ModelQuerySet"],
+) -> Callable[..., "PrimaryBoto3ModelQuerySet"]:
+    """
+    Decorator to convert a
+    :py:class:`botocraft.services.abstract.PrimaryBoto3ModelQuerySet` of
+    :py:class:`botocraft.services.docdb.DocDBCluster` objects to a
+    :py:class:`botocraft.services.abstract.PrimaryBoto3ModelQuerySet` of
+    :py:class:`botocraft.services.docdb.DocDBCluster` objects with tags
+    included.
+    """
+
+    @wraps(func)
+    def wrapper(self, *args, **kwargs) -> "PrimaryBoto3ModelQuerySet":
+        from botocraft.services.abstract import PrimaryBoto3ModelQuerySet
+
+        response = func(self, *args, **kwargs)
+        clusters: list[DocDBCluster] = []
+        for cluster in response.all():
+            tags = self.client.list_tags_for_resource(ResourceName=cluster.DBClusterArn)
+            cluster.Tags = tags["TagList"]
+            clusters.append(cluster)
+        return PrimaryBoto3ModelQuerySet(clusters)
+
+    return wrapper
+
+
+def single_docdb_instance_include_tags(
+    func: Callable[..., "DocDBInstance"],
+) -> Callable[..., "DocDBInstance"]:
+    """
+    Decorator to convert a :py:class:`botocraft.services.docdb.DocDBInstance` object
+    to a :py:class:`botocraft.services.docdb.DocDBInstance` object with tags included.
+    """
+
+    @wraps(func)
+    def wrapper(self, *args, **kwargs) -> "DocDBInstance":
+        response = func(self, *args, **kwargs)
+        tags = self.client.list_tags_for_resource(ResourceName=response.DBInstanceArn)
+        response.Tags = tags["TagList"]
+        return response
+
+    return wrapper
+
+
+def multiple_docdb_instance_include_tags(
+    func: Callable[..., "PrimaryBoto3ModelQuerySet"],
+) -> Callable[..., "PrimaryBoto3ModelQuerySet"]:
+    """
+    Decorator to convert a
+    :py:class:`botocraft.services.abstract.PrimaryBoto3ModelQuerySet` of
+    :py:class:`botocraft.services.docdb.DocDBInstance` objects to a
+    :py:class:`botocraft.services.abstract.PrimaryBoto3ModelQuerySet` of
+    :py:class:`botocraft.services.docdb.DocDBInstance` objects with tags
+    included.
+    """
+
+    @wraps(func)
+    def wrapper(self, *args, **kwargs) -> "PrimaryBoto3ModelQuerySet":
+        from botocraft.services.abstract import PrimaryBoto3ModelQuerySet
+
+        response = func(self, *args, **kwargs)
+        instances: list[DocDBInstance] = []
+        for instance in response.all():
+            tags = self.client.list_tags_for_resource(
+                ResourceName=instance.DBInstanceArn
+            )
+            instance.Tags = tags["TagList"]
+            instances.append(instance)
+        return PrimaryBoto3ModelQuerySet(instances)
+
+    return wrapper
+
+
+def single_docdb_subnet_group_include_tags(
+    func: Callable[..., "DocDBSubnetGroup"],
+) -> Callable[..., "DocDBSubnetGroup"]:
+    """
+    Decorator to convert a :py:class:`botocraft.services.docdb.DocDBSubnetGroup`
+    object to a :py:class:`botocraft.services.docdb.DocDBSubnetGroup` object
+    with tags included.
+    """
+
+    @wraps(func)
+    def wrapper(self, *args, **kwargs) -> "DocDBSubnetGroup":
+        response = func(self, *args, **kwargs)
+        tags = self.client.list_tags_for_resource(
+            ResourceName=response.DBSubnetGroupArn
+        )
+        response.Tags = tags["TagList"]
+        return response
+
+    return wrapper
+
+
+def multiple_docdb_subnet_group_include_tags(
+    func: Callable[..., "PrimaryBoto3ModelQuerySet"],
+) -> Callable[..., "PrimaryBoto3ModelQuerySet"]:
+    """
+    Decorator to convert a
+    :py:class:`botocraft.services.abstract.PrimaryBoto3ModelQuerySet` of
+    :py:class:`botocraft.services.docdb.DocDBSubnetGroup` objects to a
+    :py:class:`botocraft.services.abstract.PrimaryBoto3ModelQuerySet` of
+    :py:class:`botocraft.services.docdb.DocDBSubnetGroup` objects with tags
+    included.
+    """
+
+    @wraps(func)
+    def wrapper(self, *args, **kwargs) -> "PrimaryBoto3ModelQuerySet":
+        from botocraft.services.abstract import PrimaryBoto3ModelQuerySet
+
+        response = func(self, *args, **kwargs)
+        subnet_groups: list[DocDBSubnetGroup] = []
+        for subnet_group in response.all():
+            tags = self.client.list_tags_for_resource(
+                ResourceName=subnet_group.DBSubnetGroupArn
+            )
+            subnet_group.Tags = tags["TagList"]
+            subnet_groups.append(subnet_group)
+        return PrimaryBoto3ModelQuerySet(subnet_groups)
+
+    return wrapper
+
+
+def single_docdb_parameter_group_include_tags(
+    func: Callable[..., "DocDBParameterGroup"],
+) -> Callable[..., "DocDBParameterGroup"]:
+    """
+    Decorator to convert a :py:class:`botocraft.services.docdb.DocDBParameterGroup`
+    object to a :py:class:`botocraft.services.docdb.DocDBParameterGroup` object
+    with tags included.
+    """
+
+    @wraps(func)
+    def wrapper(self, *args, **kwargs) -> "DocDBParameterGroup":
+        response = func(self, *args, **kwargs)
+        tags = self.client.list_tags_for_resource(
+            ResourceName=response.DBParameterGroupArn
+        )
+        response.Tags = tags["TagList"]
+        return response
+
+    return wrapper
+
+
+def multiple_docdb_parameter_group_include_tags(
+    func: Callable[..., "PrimaryBoto3ModelQuerySet"],
+) -> Callable[..., "PrimaryBoto3ModelQuerySet"]:
+    """
+    Decorator to convert a
+    :py:class:`botocraft.services.abstract.PrimaryBoto3ModelQuerySet` of
+    :py:class:`botocraft.services.docdb.DocDBParameterGroup` objects to a
+    :py:class:`botocraft.services.abstract.PrimaryBoto3ModelQuerySet` of
+    :py:class:`botocraft.services.docdb.DocDBParameterGroup` objects with tags
+    included.
+    """
+
+    @wraps(func)
+    def wrapper(self, *args, **kwargs) -> "PrimaryBoto3ModelQuerySet":
+        from botocraft.services.abstract import PrimaryBoto3ModelQuerySet
+
+        response = func(self, *args, **kwargs)
+        parameter_groups: list[DocDBParameterGroup] = []
+        for parameter_group in response.all():
+            tags = self.client.list_tags_for_resource(
+                ResourceName=parameter_group.DBParameterGroupArn
+            )
+            parameter_group.Tags = tags["TagList"]
+            parameter_groups.append(parameter_group)
+        return PrimaryBoto3ModelQuerySet(parameter_groups)
+
+    return wrapper
+
+
+# ----------
+# Mixins
+# ----------
+
+
+class DocDBClusterModelMixin:
+    """
+    A mixin for :py:class:`botocraft.services.docdb.DocDBCluster` that adds
+    some additional methods that we can't auto generate.
+    """
+
+    @property
+    def credentials(self) -> "Credentials":
+        """
+        Return the master user secret for the RDS instance.
+
+        Raises:
+            ValueError: If the RDS instance does not have a master user password
+                in Secrets Manager.
+
+        Returns:
+            The master user secret for the RDS instance.
+
+        """
+        from botocraft.services.common import Credentials
+        from botocraft.services.secretsmanager import Secret
+
+        if not self.MasterUserSecret:
+            msg = (
+                f"DocumentDB instance {self.DBClusterIdentifier} does not have a "
+                "master user password in Secrets Manager"
+            )
+            raise ValueError(msg)
+        secret = Secret.objects.get(SecretId=self.MasterUserSecret.SecretArn)
+        value = secret.get_value()
+        data = json.loads(value.SecretString)
+        return Credentials(username=data["username"], password=data["password"])
