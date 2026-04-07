63163dd525593725e0bad1acb6b99e88f5123b99
Chris Malek <cmalek@caltech.edu>
2026-04-07 14:48:04 -0700

    BUGFIX: LoadBalancer.objects.get() now works

--- botocraft/mixins/elbv2.py
+++ botocraft/mixins/elbv2.py
@@ -2,7 +2,10 @@ from functools import wraps
 from typing import TYPE_CHECKING, Any, Callable
 
 if TYPE_CHECKING:
+    import builtins
+
     from botocraft.services.abstract import PrimaryBoto3ModelQuerySet
+    from botocraft.services.elbv2 import LoadBalancer
 
 
 # ----------
@@ -28,3 +31,54 @@ def load_balancer_attributes_to_dict(
         return _attrs
 
     return wrapper
+
+
+# ----------
+# Mixins
+# ----------
+
+
+class LoadBalancerManagerMixin:
+    """
+    Mixin for ELBv2 load balancers that adds attributes to the load balancer.
+    """
+
+    def get(
+        self,
+        *,
+        LoadBalancerArn: "str | None" = None,  # noqa: N803
+        Name: "str | None" = None,  # noqa: N803
+    ) -> "LoadBalancer | None":
+        """
+        Describes the specified load balancers or all of your load balancers.
+
+        Keyword Args:
+            LoadBalancerArn: The Amazon Resource Name (ARN) of the load balancer.
+            Name: The name of the load balancer.
+
+        Returns:
+            A :py:class:`LoadBalancer` object, or :py:data:`None` if no load
+            balancer is found.
+
+        """
+        from botocraft.services.elbv2 import DescribeLoadBalancersOutput
+
+        if LoadBalancerArn and Name:
+            msg = "LoadBalancerArn and Name cannot both be provided"
+            raise ValueError(msg)
+        if not LoadBalancerArn and not Name:
+            msg = "LoadBalancerArn or Name must be provided"
+            raise ValueError(msg)
+        if LoadBalancerArn:
+            args = {"LoadBalancerArns": self.serialize([LoadBalancerArn])}
+        else:
+            args = {"Names": self.serialize([Name])}
+        _response = self.client.describe_load_balancers(
+            **{k: v for k, v in args.items() if v is not None}
+        )
+        response = DescribeLoadBalancersOutput(**_response)
+
+        if response and response.LoadBalancers:
+            self.sessionize(response.LoadBalancers[0])
+            return response.LoadBalancers[0]
+        return None
